f1483bc5fcfe163d02d49d0809ec6518dd507694
Avery McLean <mcleanavery04@gmail.com>
2025-05-23 19:13:17 -0400

    Coupla medium questions.

---
+++ Medium/longestSubstring.py
@@ -0,0 +1,18 @@
+class Solution(object):
+    def lengthOfLongestSubstring(self, s):
+        """
+        :type s: str
+        :rtype: int
+        """
+        char_set = set()
+        left = 0
+        result = 0
+
+        for right in range(len(s)):
+            while s[right] in char_set:
+                char_set.remove(s[left])
+                left += 1
+            char_set.add(s[right])
+            result = max(result, right - left + 1)
+
+        return result
